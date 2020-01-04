2a7fad1109589d6de0d9ab668a5f586007e75868
yourpapaji <shubhkarman.socials@gmail.com>
2020-01-04 21:46:45 +0530

    update

--- suckless/.local/src/dwm/config.h
+++ suckless/.local/src/dwm/config.h
@@ -9,12 +9,12 @@ static const unsigned int gappoh    = 16;       /* horiz outer gap between windo
 static const unsigned int gappov    = 16;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const unsigned int systrayspacing = 1;   /* systray spacing */
+static const int systraypinningfailfirst = 0;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "FuraCode Nerd Font:style=Regular:pixelsize=12:antialias=true:autohint=false", "Font Awesome Free:pixelsize=12" };
+static const char *fonts[]          = { "FuraCode Nerd Font:style=Regular:pixelsize=12:antialias=true:autohint=false", "Font Awesome Brands:pixelsize=12", "Font Awesome Free:pixelsize=12" };
 static const char col_gray1[]       = "#1d2021";
 static const char col_gray2[]       = "#458588";
 static const char col_gray3[]       = "#928374";
