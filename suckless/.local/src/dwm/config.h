2d015309ac65f79fb89bba82460de988bf49e6bd
yourpapaji <shubhkarman.socials@gmail.com>
2020-02-17 11:21:01 +0530

    dwm update

--- suckless/.local/src/dwm/config.h
+++ suckless/.local/src/dwm/config.h
@@ -3,10 +3,10 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 8;        /* snap pixel */
-static const unsigned int gappih    = 4;        /* horiz inner gap between windows */
-static const unsigned int gappiv    = 4;        /* vert inner gap between windows */
-static const unsigned int gappoh    = 8;        /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 8;        /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 6;        /* horiz inner gap between windows */
+static const unsigned int gappiv    = 6;        /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
